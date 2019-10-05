6eb6142cb1c5fce7cb2677f84acf54d9823561b8
gabrielledelforge <gabrielle.delforge@berkeley.edu>
2019-10-05 15:02:00 -0700

    fix tiled file paths

---
+++ Assets/Map/tiles.tsx
@@ -0,0 +1,108 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="tiles" tilewidth="256" tileheight="256" tilecount="20" columns="4">
+ <image source="../Graphics/tileset1.png" width="1024" height="1280"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="1" x="262" y="32">
+    <polygon points="0,0 -163.798,17.3333 -230,52.6667 -230,97.3333 -0.682493,98"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index">
+   <object id="3" x="-4" y="31.3333" width="264" height="98.6667"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index">
+   <object id="1" x="-4.66667" y="33.3333">
+    <polygon points="0,0 96.5281,2.52903 239.281,46.7871 242,97.3677 0.679775,98"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index">
+   <object id="7" x="34.6667" y="47.3333" width="179.333" height="90.6667"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index">
+   <object id="1" x="18" y="77.3333" width="239.333" height="153.333"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index">
+   <object id="1" x="-2.66667" y="77.3333" width="264" height="153.333"/>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index">
+   <object id="1" x="-6" y="75.3333" width="246" height="152"/>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index">
+   <object id="1" x="40" y="82.6667" width="170.667" height="129.333"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index">
+   <object id="1" x="19.3333" y="76.6667" width="243.333" height="181.333"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index">
+   <object id="1" x="-20" y="77.3333" width="285.333" height="190.667"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index">
+   <object id="1" x="-10" y="79.3333" width="232" height="181.333"/>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <objectgroup draworder="index">
+   <object id="1" x="40.6667" y="83.3333" width="172" height="182"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index">
+   <object id="1" x="21.3333" y="-4.66667" width="244.667" height="271.333"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index">
+   <object id="1" x="-6" y="-6" width="270" height="266.667"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <objectgroup draworder="index">
+   <object id="1" x="-6" y="-6" width="226.667" height="266.667"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index">
+   <object id="1" x="32" y="-9.33333" width="198.667" height="280.667"/>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup draworder="index">
+   <object id="1" x="24.6667" y="-4.66667" width="237.333" height="244.667"/>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <objectgroup draworder="index">
+   <object id="1" x="-6" y="-8" width="272" height="244"/>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <objectgroup draworder="index">
+   <object id="1" x="-8" y="-2.66667" width="229.333" height="236"/>
+  </objectgroup>
+ </tile>
+ <tile id="19">
+  <objectgroup draworder="index">
+   <object id="1" x="34" y="-10.6667" width="190" height="238.667"/>
+  </objectgroup>
+ </tile>
+</tileset>
