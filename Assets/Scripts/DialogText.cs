06f3016724db4daef1c092d443ede9339f06d84d
johnfn <johnfn@gmail.com>
2019-10-05 19:17:18 -0700

    Modes

--- Assets/Scripts/DialogText.cs
+++ Assets/Scripts/DialogText.cs
@@ -15,5 +15,6 @@ public class DialogText {
   public static List<DialogItem> FirstDialog = new List<DialogItem> {
     new DialogItem { Name = CharacterName.Blank, Contents = "Hi there" },
     new DialogItem { Name = CharacterName.Ash, Contents = "What's up?" },
+    new DialogItem { Name = CharacterName.Blank, Contents = "nm nm hbu" },
   };
 }
