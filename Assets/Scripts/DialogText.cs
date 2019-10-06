7eec7939990da28347e18419bca2ebdf2f911cc3
johnfn <johnfn@gmail.com>
2019-10-06 13:17:21 -0700

    Some dialog stuf

--- Assets/Scripts/DialogText.cs
+++ Assets/Scripts/DialogText.cs
@@ -25,27 +25,29 @@ public class DialogEvent {
 }
 
 public class DialogText {
+  public static string CuriosityColor = "#6699ff";
+
   public static List<DialogEvent> FirstDialog = new List<DialogEvent> {
     new DialogEvent { Fade = new Fade { Immediate = true, RectangleFadeOpacity = 1.0f } },
 
-    new DialogEvent { Name = CharacterName.Ash  , Contents = "Hey!" },
+    new DialogEvent { Name = CharacterName.Ash  , Contents = $"<color={ CuriosityColor }>Hey!</color>" },
     new DialogEvent { Name = CharacterName.Blank, Contents = "..." },
 
     new DialogEvent { ReceiveEmotion = EmotionType.Curiosity },
 
-    new DialogEvent { Name = CharacterName.Ash  , Contents = "What happened? Are you okay?" },
+    new DialogEvent { Name = CharacterName.Ash  , Contents = $"What happened? Are you okay?" },
     new DialogEvent { Name = CharacterName.Blank, Contents = "..." },
-    new DialogEvent { Name = CharacterName.Ash  , Contents = "What’s your name?" },
+    new DialogEvent { Name = CharacterName.Ash  , Contents = "What's your name?" },
     new DialogEvent { Name = CharacterName.Blank, Contents = "..." },
-    new DialogEvent { Name = CharacterName.Ash  , Contents = "I haven’t seen someone down here in ages…" },
+    new DialogEvent { Name = CharacterName.Ash  , Contents = "I haven't seen someone down here in ages…" },
     new DialogEvent { Name = CharacterName.Blank, Contents = "..." },
     new DialogEvent { Name = CharacterName.Ash  , Contents = "And wow, you can tell. What a stench! Whew!" },
     new DialogEvent { Name = CharacterName.Blank, Contents = "..." },
     new DialogEvent { Name = CharacterName.Ash  , Contents = "Hello? Cat got your tongue? Can you understand me?" },
     new DialogEvent { Name = CharacterName.Blank, Contents = "..." },
-    new DialogEvent { Name = CharacterName.Ash  , Contents = "Hey, we can’t figure out what’s going on until you talk to me. Aren't you curious, too?" },
+    new DialogEvent { Name = CharacterName.Ash  , Contents = "Hey, we can’t figure out what’s going on until you talk to me. Aren't you <color=#6699ff>curious</color>, too?" },
     new DialogEvent { Fade = new Fade { RectangleFadeOpacity = 0.9f, } }, //new DialogEvent { Fade = new Fade { RectangleFadeOpacity = 0.9f, CircleFadeOpacity = 0.5f } },
-    new DialogEvent { Name = CharacterName.Blank, Contents = "...curious?..." },
+    new DialogEvent { Name = CharacterName.Blank, Contents = $"<color={ CuriosityColor }>...curious?...</color>" },
     new DialogEvent { Fade = new Fade { RectangleFadeOpacity = 0.7f } }, //new DialogEvent { Fade = new Fade { RectangleFadeOpacity = 0.7f, CircleFadeOpacity = 0.5f } },
     new DialogEvent { Name = CharacterName.Blank, Contents = "W..." },
     new DialogEvent { Name = CharacterName.Blank, Contents = "...where am I?" },
