31b36e10eb43b21e27e0581e8b172b80f8977580
Oasiris <teracontinens@gmail.com>
2019-10-07 12:58:45 -0700

    Interactable book dialog finished

--- Assets/Scripts/EmotionInteractableBooks.cs
+++ Assets/Scripts/EmotionInteractableBooks.cs
@@ -20,11 +20,33 @@ public class EmotionInteractableBooks: EmotionInteractable {
                     new DialogEvent { Name = CharacterName.Blank, Contents = "The signature at the bottom has been ripped off." },
                 }
             );
+        } else if (Lens.Instance.ActiveEmotion == EmotionType.Compassion) {
+            DialogManager.Instance.StartDialogSequence(
+                new List<DialogEvent> {
+                    new DialogEvent { Name = CharacterName.Blank, Contents = "As you smooth out the paper, you wonder if the poem was written for Ash." },
+                    new DialogEvent { Name = CharacterName.Blank, Contents = "​​You can feel the author’s overpowering feelings washing over your body.", },
+                }
+            );
+        } else if (Lens.Instance.ActiveEmotion == EmotionType.Affection) {
+            DialogManager.Instance.StartDialogSequence(
+                new List<DialogEvent> {
+                    new DialogEvent { Name = CharacterName.Blank, Contents = "​​The kindness and tenderness of the poem’s prose jumps out at you." },
+                    new DialogEvent { Name = CharacterName.Blank, Contents = "​​Who wrote this poem for Ash? Unless…", },
+                }
+            );
+        } else if (Lens.Instance.ActiveEmotion == EmotionType.Remorse) {
+            DialogManager.Instance.StartDialogSequence(
+                new List<DialogEvent> {
+                    new DialogEvent { Name = CharacterName.Blank, Contents = "​​As you finger over the delicate corners of the pages, your heart feels heavy." },
+                    new DialogEvent { Name = CharacterName.Blank, Contents = "​​The gravity of the author’s lament hits you at once.", },
+                    new DialogEvent { Name = CharacterName.Blank, Contents = "​​​​You suddenly realize you are a voyeur to someone else’s deeply personal work.", },
+                }
+            );
         } else {
             DialogManager.Instance.StartDialogSequence(
                 new List<DialogEvent> {
                     new DialogEvent { Name = CharacterName.Blank, Contents = "It's some old books." },
-                    new DialogEvent { Name = CharacterName.Blank, Contents = "Nothing too interesting." }
+                    new DialogEvent { Name = CharacterName.Blank, Contents = "Doesn't look too interesting." }
                 }
             );
         }
