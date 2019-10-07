cb3e3569a9c2c3b813c3754bf1252aa31caf4e64
Tiffany <cnnmonn@gmail.com>
2019-10-07 16:40:12 -0400

    credits

---
+++ Assets/Scripts/CreditManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditManager : MonoBehaviour
+{
+    public GameObject creditContent;
+
+    void Start()
+    {
+        LeanTween.moveLocalY(creditContent, 1640, 20f).setOnComplete(() => {
+            PlayerPrefs.SetInt("FinishedGame", 1);
+            SceneManager.LoadScene("Title");
+        });
+    }
+
+    
+}
