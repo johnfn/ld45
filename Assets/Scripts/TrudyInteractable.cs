70a6fcff8578541153842519d84013c321c955b1
Tiffany <cnnmonn@gmail.com>
2019-10-07 00:18:05 -0400

    interactable

--- Assets/Scripts/TrudyInteractable.cs
+++ Assets/Scripts/TrudyInteractable.cs
@@ -5,7 +5,8 @@ public class TrudyInteractable: Interactable {
   override public void ShowAsInteractable() {
       interactIndicator = Instantiate(Manager.Instance.interactIndicatorPrefab, new Vector3(xPos, transform.position.y + 3, 0), Quaternion.identity, Manager.Instance.interactIndicatorParent);
       interactIndicator.SetActive(true);
-  }
+      Manager.Instance.SetInstruction("Press X to interact.");
+    }
 
   override public void Interact() {
     DialogManager.Instance.StartDialogSequence(DialogText.Trudialog);
