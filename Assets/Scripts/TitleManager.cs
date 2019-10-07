082fc49d60fa7ee5df2fcc0cc6f8ea1291e904d1
Tiffany <cnnmonn@gmail.com>
2019-10-06 23:34:58 -0400

    title

--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -1,18 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class TitleManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject title;
+    public Sprite colorfulTitle;
+    public CanvasGroup errorMsg;
+    public CanvasGroup content;
+
+    public TMP_InputField inputField;
+    public Button button;
+    public CanvasGroup allCanvas;
+
     void Start()
     {
-        
+        if (PlayerPrefs.GetInt("FinishedGame", 0) == 1) title.GetComponent<Image>().sprite = colorfulTitle;
+        StartAnim();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void StartAnim()
     {
-        
+        LeanTween.scale(title, Vector3.one, 2f).setEaseInOutQuad().setOnComplete(() => {
+            LeanTween.moveLocalY(title, 110, 1f).setEaseInOutQuad();
+            LeanTween.alphaCanvas(content, 1, 1f).setEaseInOutQuad();
+        });
+    }
+
+    public void LimitInput()
+    {
+        inputField.text = "";
+        if (errorMsg.alpha == 0)
+        {
+            LeanTween.alphaCanvas(errorMsg, 1, 0.7f);
+            button.interactable = true;
+        }
+    }
+
+    public void GoTo()
+    {
+
+        LeanTween.alphaCanvas(allCanvas, 0, 1f).setEaseInOutQuad().setOnComplete(() => { SceneManager.LoadScene("SampleScene"); });
     }
 }
